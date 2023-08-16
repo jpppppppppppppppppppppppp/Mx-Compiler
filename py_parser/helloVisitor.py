bdb737a2dadf24f324ea2eeb9c0a67614be2c8fb
jpppppppppppppppppppppppp <1490881945@qq.com>
2023-08-16 15:19:26 +0800

    2023.8.15 -- end easy assembly

--- py_parser/helloVisitor.py
+++ py_parser/helloVisitor.py
@@ -1,6 +1,6 @@
-# Generated from C:/Users/14908/Desktop/PPCA/Compiler\hello.g4 by ANTLR 4.12.0
+# Generated from C:/Users/14908/Desktop/PPCA/Compiler\hello.g4 by ANTLR 4.13.0
 from antlr4 import *
-if __name__ is not None and "." in __name__:
+if "." in __name__:
     from .helloParser import helloParser
 else:
     from helloParser import helloParser
